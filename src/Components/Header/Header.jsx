1c38292be40f8b7e687a7880acf35a81160f7ed0
Omar Castro <omarsc97@outlook.com>
2022-04-25 15:16:32 -0500

    Creando componentes

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+    return{
+        <nav>
+            <div className="nav-wrapper light-blue darken-2">
+                <a href="#!" className="brand-logo">{titulo}</a>
+                
+            </div>
+        </nav>
+    }
+}
