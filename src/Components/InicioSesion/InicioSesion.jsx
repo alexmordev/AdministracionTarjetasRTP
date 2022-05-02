0cb40d30d66463aee4331ffdd43166cf0bb710c9
Omar Castro <omarsc97@outlook.com>
2022-05-02 17:39:36 -0500

    Añadi multiboton en homepage

--- src/Components/InicioSesion/InicioSesion.jsx
+++ src/Components/InicioSesion/InicioSesion.jsx
@@ -10,7 +10,7 @@ export  const InicioSesion =() =>{
               id='nombre'
               type="text" 
               placeholder='Nombre del usuario'
-              className='border-2 w-96 h-20 hover:h-24 hover:w-102 p-2 mt-2  rounded-md font-bold' 
+              className='border-2 w-96 h-20  p-2 mt-2  rounded-md font-bold' 
               />
 
               <br /><br /> <br />
@@ -19,7 +19,7 @@ export  const InicioSesion =() =>{
               id='contraseña'
               type="text"
               placeholder='Contraseña'
-              className='border-2 w-96 h-20 hover:h-24 hover:w-102 p-2 mt-2 placeholder-gray-400 rounded-md font-bold'>
+              className='border-2 w-96 h-20  p-2 mt-2 placeholder-gray-400 rounded-md font-bold'>
               </input>
               <br></br>
               <br></br> <br />
@@ -28,11 +28,11 @@ export  const InicioSesion =() =>{
               <input type="submit"
               className='bg-green-600 w-40 p-3 
                text-white uppercase font-bold
-               hover:bg-green-700 cursor-pointer rounded-lg flex justify-center hover:w-44 hover:h-20'
+               hover:bg-green-700 cursor-pointer rounded-lg flex justify-center'
               value="Registro" />            
               <br></br><br />
               <a  className='text-decoration-line: underline text-white font-bold
-              hover:text-blue-600 hover:font-extrabold hover:text-xl'   href="Homepage.jsx">¿Olvidaste tu contraseña?</a>
+              hover:text-blue-600 hover:font-extrabold hover:text-lg'   href="Homepage.jsx">¿Olvidaste tu contraseña?</a>
             </div>
           </form>
           // <div className='line-through'>Log in</div>
