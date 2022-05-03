6d1a217f0839a46dc847bde43d50349e9b9e99e4
Daniel Ramírez <mercurial1916@hotmail.com>
2022-05-03 17:08:51 -0500

    ultimos cambios en rama alterna

--- src/Components/InicioSesion/Input_usuario/Input_usuario.jsx
+++ src/Components/InicioSesion/Input_usuario/Input_usuario.jsx
@@ -2,20 +2,11 @@ import React from 'react'
 
 export const Input_usuario = () => {
   return (
-<<<<<<< HEAD
-    <input
-          id="nombre"
-          type="text"
-          placeholder="Nombre del usuario"
-          className="border-2 w-50 p-2 mt-2  rounded-md font-bold"
-        />
-=======
     <input 
               id='nombre'
               type="text" 
               placeholder='Nombre del usuario'
               className='border-2 w-50 p-2 mt-2  rounded-md font-bold' 
               />
->>>>>>> af7dc3d166a3bee374f74a3dc739862e03cfafda
   )
 }
