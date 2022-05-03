2d13d0704a6107e694ed7c21d5899bb8229a5602
Omar Castro <omarsc97@outlook.com>
2022-05-03 17:04:23 -0500

    Cambios en multi boton

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import "../../index.css"
-import {Boton} from '../../Components/InicioSesion/Boton/Boton'
-import {Input_contra} from '../../Components/InicioSesion/Input_Contra/Input_contra'
-import {Input_usuario} from '../../Components/InicioSesion/Input_usuario/Input_usuario'
 import { InicioSesion } from '../../Components/InicioSesion/InicioSesion'
 
 
